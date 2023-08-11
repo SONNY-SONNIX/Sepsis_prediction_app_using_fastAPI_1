e13fe9ba56691270d1a28db8cd5e23d0706c99e5
Sonny Agorvor-Otchie <Sonny4Sonnix@users.noreply.huggingface.co>
2023-08-11 04:47:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,76 @@
+# from fastapi import FastAPI, Query, Request, HTTPException
+# from fastapi.responses import HTMLResponse
+# from fastapi.templating import Jinja2Templates
+# import joblib 
+
+# app = FastAPI()
+# templates = Jinja2Templates(directory="templates")
+
+# # Load the pickled XGBoost model
+# xgb_model = joblib.load("xgb_model.joblib")
+
+# @app.get("/display")
+# def display_params(
+#     request: Request,
+#     prg: float = Query(..., description="Plasma glucose"),
+#     pl: float = Query(..., description="Blood Work Result-1 (mu U/ml)"),
+#     pr: float = Query(..., description="Blood Pressure (mm Hg)"),
+#     sk: float = Query(..., description="Blood Work Result-2 (mm)"),
+#     ts: float = Query(..., description="Blood Work Result-3 (mu U/ml)"),
+#     m11: float = Query(..., description="Body mass index (weight in kg/(height in m)^2"),
+#     bd2: float = Query(..., description="Blood Work Result-4 (mu U/ml)"),
+#     age: int = Query(..., description="Patient's age (years)")
+# ):
+#     try:
+#         # Prepare input features for prediction
+#         input_features = [prg, pl, pr, sk, ts, m11, bd2, age]
+
+#         # Make predictions using the loaded model
+#         prediction = xgb_model.predict_proba([input_features])[0]
+
+#         # Create a JSON response 
+#         response = {
+#             "request": {
+#                 "prg": prg,
+#                 "pl": pl,
+#                 "pr": pr,
+#                 "sk": sk,
+#                 "ts": ts,
+#                 "m11": m11,
+#                 "bd2": bd2,
+#                 "age": age
+#             },
+#             "prediction": {
+#                 "class_0_probability": prediction[0],
+#                 "class_1_probability": prediction[1]               
+#             }
+#         }
+
+#         return templates.TemplateResponse(
+#             "display_params.html",
+#             {
+#                 "request": request,
+#                 "prg": prg,
+#                 "pl": pl,
+#                 "pr": pr,
+#                 "sk": sk,
+#                 "ts": ts,
+#                 "m11": m11,
+#                 "bd2": bd2,
+#                 "age": age,
+#                 "prediction": response["prediction"]
+#             }
+#         )
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
+
+
+
+
 from fastapi import FastAPI, Query, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import joblib 
+import joblib
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -9,8 +78,12 @@ templates = Jinja2Templates(directory="templates")
 # Load the pickled XGBoost model
 xgb_model = joblib.load("xgb_model.joblib")
 
-@app.get("/display")
-def display_params(
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the XGBoost Diabetes Prediction API"}
+
+@app.get("/predict/")
+async def predict_diabetes(
     request: Request,
     prg: float = Query(..., description="Plasma glucose"),
     pl: float = Query(..., description="Blood Work Result-1 (mu U/ml)"),
@@ -28,7 +101,7 @@ def display_params(
         # Make predictions using the loaded model
         prediction = xgb_model.predict_proba([input_features])[0]
 
-        # Create a JSON response 
+        # Create a JSON response
         response = {
             "request": {
                 "prg": prg,
@@ -42,7 +115,7 @@ def display_params(
             },
             "prediction": {
                 "class_0_probability": prediction[0],
-                "class_1_probability": prediction[1]               
+                "class_1_probability": prediction[1]
             }
         }
 
@@ -50,16 +123,10 @@ def display_params(
             "display_params.html",
             {
                 "request": request,
-                "prg": prg,
-                "pl": pl,
-                "pr": pr,
-                "sk": sk,
-                "ts": ts,
-                "m11": m11,
-                "bd2": bd2,
-                "age": age,
+                "input_features": response["request"],
                 "prediction": response["prediction"]
             }
         )
     except Exception as e:
         raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
+
