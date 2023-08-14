546d5b3cf649eb68100c389b44e7d83ebe5e20b5
Sonny Agorvor-Otchie <Sonny4Sonnix@users.noreply.huggingface.co>
2023-08-14 12:52:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -162,6 +162,13 @@ async def show_form():
 async def predict_ml(
     request: Request,
     prg: float = Query(..., description="Plasma glucose"),
+    pl: float = Query(..., description="Blood Work Result-1 (mu U/ml)"),
+    pr: float = Query(..., description="Blood Pressure (mm Hg)"),
+    sk: float = Query(..., description="Blood Work Result-2 (mm)"),
+    ts: float = Query(..., description="Blood Work Result-3 (mu U/ml)"),
+    m11: float = Query(..., description="Body mass index (weight in kg/(height in m)^2"),
+    bd2: float = Query(..., description="Blood Work Result-4 (mu U/ml)"),
+    age: int = Query(..., description="Patient's age (years)")
     # ... (other input parameters)
 ):
     try:
