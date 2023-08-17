1ae8c739a15cc81d054c2f1df393d72df31b0846
Sonny Agorvor-Otchie <Sonny4Sonnix@users.noreply.huggingface.co>
2023-08-17 08:59:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,141 +1,7 @@
-# from fastapi import FastAPI, Query, Request, HTTPException
-# from fastapi.responses import HTMLResponse
-# from fastapi.templating import Jinja2Templates
-# import joblib 
-
-# app = FastAPI()
-# templates = Jinja2Templates(directory="templates")
-
-# # Load the pickled XGBoost model
-# xgb_model = joblib.load("xgb_model.joblib")
-
-# @app.get("/display")
-# def display_params(
-#     request: Request,
-#     prg: float = Query(..., description="Plasma glucose"),
-#     pl: float = Query(..., description="Blood Work Result-1 (mu U/ml)"),
-#     pr: float = Query(..., description="Blood Pressure (mm Hg)"),
-#     sk: float = Query(..., description="Blood Work Result-2 (mm)"),
-#     ts: float = Query(..., description="Blood Work Result-3 (mu U/ml)"),
-#     m11: float = Query(..., description="Body mass index (weight in kg/(height in m)^2"),
-#     bd2: float = Query(..., description="Blood Work Result-4 (mu U/ml)"),
-#     age: int = Query(..., description="Patient's age (years)")
-# ):
-#     try:
-#         # Prepare input features for prediction
-#         input_features = [prg, pl, pr, sk, ts, m11, bd2, age]
-
-#         # Make predictions using the loaded model
-#         prediction = xgb_model.predict_proba([input_features])[0]
-
-#         # Create a JSON response 
-#         response = {
-#             "request": {
-#                 "prg": prg,
-#                 "pl": pl,
-#                 "pr": pr,
-#                 "sk": sk,
-#                 "ts": ts,
-#                 "m11": m11,
-#                 "bd2": bd2,
-#                 "age": age
-#             },
-#             "prediction": {
-#                 "class_0_probability": prediction[0],
-#                 "class_1_probability": prediction[1]               
-#             }
-#         }
-
-#         return templates.TemplateResponse(
-#             "display_params.html",
-#             {
-#                 "request": request,
-#                 "prg": prg,
-#                 "pl": pl,
-#                 "pr": pr,
-#                 "sk": sk,
-#                 "ts": ts,
-#                 "m11": m11,
-#                 "bd2": bd2,
-#                 "age": age,
-#                 "prediction": response["prediction"]
-#             }
-#         )
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
-
-
-
-
-# from fastapi import FastAPI, Query, Request, HTTPException
-# from fastapi.responses import HTMLResponse
-# from fastapi.templating import Jinja2Templates
-# import joblib
-# from sklearn.preprocessing import StandardScaler  # Import a standard scaler for data preprocessing
-
-# app = FastAPI()
-# templates = Jinja2Templates(directory="templates")
-
-# # Load the pickled XGBoost model
-# xgb_model = joblib.load("xgb_model.joblib")
-
-# # @app.get("/app")
-# # async def read_root():
-# #     return {"message": "Welcome to the XGBoost Diabetes Prediction API"}
-
-# @app.get("/predict/")
-# async def predict_diabetes(
-#     request: Request,
-#     prg: float = Query(..., description="Plasma glucose"),
-#     pl: float = Query(..., description="Blood Work Result-1 (mu U/ml)"),
-#     pr: float = Query(..., description="Blood Pressure (mm Hg)"),
-#     sk: float = Query(..., description="Blood Work Result-2 (mm)"),
-#     ts: float = Query(..., description="Blood Work Result-3 (mu U/ml)"),
-#     m11: float = Query(..., description="Body mass index (weight in kg/(height in m)^2"),
-#     bd2: float = Query(..., description="Blood Work Result-4 (mu U/ml)"),
-#     age: int = Query(..., description="Patient's age (years)")
-# ):
-#     try:
-#         # Prepare input features for prediction
-#         input_features = [prg, pl, pr, sk, ts, m11, bd2, age]
-
-#         # Make predictions using the loaded model
-#         prediction = xgb_model.predict_proba([input_features])[0]
-
-#         # Create a JSON response
-#         response = {
-#             "request": {
-#                 "prg": prg,
-#                 "pl": pl,
-#                 "pr": pr,
-#                 "sk": sk,
-#                 "ts": ts,
-#                 "m11": m11,
-#                 "bd2": bd2,
-#                 "age": age
-#             },
-#             "prediction": {
-#                 "class_0_probability": prediction[0],
-#                 "class_1_probability": prediction[1]
-#             }
-#         }
-
-#         return templates.TemplateResponse(
-#             "display_params.html",
-#             {
-#                 "request": request,
-#                 "input_features": response["request"],
-#                 "prediction": response["prediction"]
-#             }
-#         )
-#     except Exception as e:
-#        raise HTTPException(status_code=500, detail=(e))
-
 
 
 from fastapi import FastAPI, Query, Request, HTTPException
 from fastapi.responses import JSONResponse, HTMLResponse
-from fastapi.templating import Jinja2Templates
 import joblib
 import pandas as pd
 
@@ -150,12 +16,12 @@ model = joblib.load("XGB.joblib")
 
 @app.get("/")
 async def read_root():
-    return {"message": "Welcome to the Machine Learning Prediction API"}
+    return {"message": "Welcome to the Sepsis Prediction using FastAPI"}
 
-@app.get("/form/")
-async def show_form():
-#     return templates.TemplateResponse("input_form.html", {"request": None})
-    return{"Welcome to the Sepsis Prediction using FastAPdI"}
+# @app.get("/form/")
+# async def show_form():
+# #     return templates.TemplateResponse("input_form.html", {"request": None})
+#     return{"Welcome to the Sepsis Prediction using FastAPdI"}
 
 def classify(prediction):
     if prediction == 0:
@@ -198,55 +64,3 @@ async def predict_sepsis(
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=7860)
-
-    
-    
-    # try:
-    #     # Prepare input features for prediction as a DataFrame
-    #     input_data = pd.DataFrame({
-    #         "prg": [prg],
-    #         "pl": [pl],
-    #         "pr": [pr],
-    #         "sk": [sk],
-    #         "ts": [ts],
-    #         "m11": [m11],
-    #         "bd2": [bd2],
-    #         "age": [age]
-    #     })
-
-        # Scale the input data using the loaded scaler
-        #model_input = model.transform(input_data)
-
-        # Make predictions using the loaded machine learning model
-        #prediction = model.predict_proba(input_data)
-
-        # # Create a JSON response
-        # response = {
-        #     "request": {
-        #         "prg": prg,                
-        #         "pl": pl,
-        #         "pr": pr,
-        #         "sk": sk,                 
-        #         "ts": ts,
-        #         "m11": m11,
-        #         "bd2": bd2,
-        #         "age": age
-        #     },
-        #     "prediction": {
-        #         "class_0_probability": prediction[0][0],
-        #         "class_1_probability": prediction[0][1]
-        #     }
-        # }
-
-    #     return templates.TemplateResponse(
-    #         "display_params.html",
-    #         {
-    #             "request": request,
-    #             "input_features": response["request"],
-    #             "prediction": response["prediction"]
-    #         }
-    #     )
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
-
-
